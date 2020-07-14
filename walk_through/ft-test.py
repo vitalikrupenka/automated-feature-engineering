eb480097e07cf3a81036a3439b2374af40072e90
vitalikrupenka <netgigg@gmail.com>
2020-07-14 18:00:11 +0300

    new

---
+++ walk_through/ft-test.py
@@ -0,0 +1,13 @@
+import pandas as pd 
+
+loans = pd.read_csv('data/loans.csv')
+clients = pd.read_csv('data/clients.csv', low_memory=False)
+
+# Group loans by client id and calculate mean, max, min of loans
+stats = loans.groupby('client_id')['loan_amount'].agg(['mean', 'max', 'min'])
+stats.columns = ['mean_loan_amount', 'max_loan_amount', 'min_loan_amount']
+
+# Merge with the clients dataframe
+stats = clients.merge(stats, left_on = 'client_id', right_index=True, how = 'left')
+
+stats.head(10)
