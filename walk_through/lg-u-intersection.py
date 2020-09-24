2bfd1f767c343b8ffbb488458fac611e991f261a
vitalikrupenka <netgigg@gmail.com>
2020-09-24 03:52:26 +0300

    upd for predicted look images

--- walk_through/lg-u-intersection.py
+++ walk_through/lg-u-intersection.py
@@ -3,9 +3,6 @@ import pandas as pd
 u_features = pd.read_csv('data/lg_features_real.csv')
 u_looks = pd.read_csv('data/lg_looks_real.csv')
 
-# print(u_features.columns.values, u_features.shape, end='\n\n')
-# print(u_looks.columns.values, u_looks.shape, end='\n\n')
-
 uf_set = set(u_features.columns.values)
 ul_set = set(u_looks.columns.values)
 
@@ -28,21 +25,31 @@ isr = []
 for i in range(len(isf.values)):
     for j in range(len(isl.values)):
         # print(intersection(isf.values[i], isl.values[j]))
-        isr.append([i+1, j+1, 
-                    float('{:.2f}'.format(len(intersection(isf.values[i], isl.values[j])) / len(isf.values[i]) * 100))]) 
-        
-ism = {}
-mx = 0
-for i in range(len(isr)):
-    curr = isr[i][2]
-    if curr > mx:
-        mx = curr
-    ism[isr[i][0]] = [isr[i][1], mx]
-    # ism[isr[i][0]] = ism.get(isr[i][0], []).append([isr[i][1], mx])
+        if isf['male'][i] == isl['male'][j]:
+            isr.append([i+1, j+1, 
+                        float('{:.2f}'.format(len(intersection(isf.values[i], isl.values[j])) / len(isf.values[i]) * 100))]) 
 
-print(mx)
-# print(isr[:10])
+# print(isr)
 
-for key, val in ism.items():
-    print('User\'s %d favorite Look is %d with the probability of %.2f' % (key, val[0], val[1]), end='\n')
-print(ism.keys(), len(ism.keys()))
+ism = dict()
+for item in isr:
+    ism[item[0]] = ism.get(item[0], [0, 0, 0])
+    if item[2] > ism[item[0]][1]:
+        ism[item[0]] = ((item[1], item[2]))
+
+print(ism)
+
+# print(isf['male'][11])
+# print(isl.values[14])
+
+# for key, val in ism.items():
+#     print('User\'s %d favorite Look is %d with the probability of %.2f' % (key, val[0], val[1]), end='\n')
+
+import matplotlib.image as img 
+import matplotlib.pyplot as plt 
+
+user_id = 32
+
+look_img = img.imread('data/looks/%d.png' % (ism[user_id][0]))
+plt.imshow(look_img)
+plt.show()
